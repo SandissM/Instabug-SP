2ffc9aea5ab13925461a25ac3eb5080f3a50ec32
Sandis Millers <sandis@chi.lv>
2023-06-15 13:44:09 +0300

    Fixed checksum issues

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .binaryTarget(
             name: "Instabug",
             url: "https://github.com/Instabug/Instabug-SP/releases/download/11.13.1/Instabug-XCFramework.zip",
-            checksum: "bcae31c350c043e00631999d0f55320d92efa780d0ec9b9c674a5c63b9dc5945"),
+            checksum: "55bc9f00dd0e58f608c3701d22f20eb8fe94bc013341ac2ef3739c41f252d8ae"),
         .target(name: "_InstabugStub"),
     ]
 )
